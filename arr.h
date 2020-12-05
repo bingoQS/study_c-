c301c7a10e91ec947e8e5ca885d33131d045c505
bingoQS <75433565+bingoQS@users.noreply.github.com>
2020-12-05 11:47:51 +0800

    Create arr.h

---
+++ arr.h
@@ -0,0 +1,13 @@
+#pragma once
+
+//1.遍历数组
+void printfArr(int *, int);
+
+//2.计算数组元素的和
+int getSum(int *, int);
+
+//3.获取数组最值
+void getMax(int *, int,int *);
+
+//4.对数组排序，冒泡
+void bubbleSort(int *, int );
